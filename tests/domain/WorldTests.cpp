71e1e3a35b6d762181f7e91404945b1f1778b3e4
IagoGdeMeira <iagomeiraifpr@gmail.com>
2026-02-13 08:48:05 -0300

    Criando testes falsos para testar CI.

---
+++ tests/domain/WorldTests.cpp
@@ -0,0 +1,11 @@
+#ifndef world_tests_h
+#define world_tests_h
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Dummy domain test")
+{
+    REQUIRE(1 + 1 == 2);
+}
+
+#endif // world_tests_h
---
+++ tests/engine/EventBusTests.cpp
@@ -0,0 +1,11 @@
+#ifndef event_bus_tests_h
+#define event_bus_tests_h
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Dummy engine test")
+{
+    REQUIRE(true);
+}
+
+#endif // event_bus_tests_h
