bb92710d16be56a64f37e833daa8dc50bad946db
IagoGdeMeira <iagomeiraifpr@gmail.com>
2026-02-20 00:13:09 -0300

    fix(domain): corrige inconsistências nos testes de ComponentType

--- tests/domain/ComponentTypeTests.cpp
+++ tests/domain/ComponentTypeTests.cpp
@@ -2,17 +2,14 @@
 
 #include <catch2/catch_test_macros.hpp>
 
-struct Position {};
-struct Velocity {};
-struct Health {};
-struct Special {};
-
 TEST_CASE(
     "ComponentType returns same id for same type",
     "[unit][component_type]"
 ) {
-    size_t id1 = ComponentType::id<Position>();
-    size_t id2 = ComponentType::id<Position>();
+    struct LocalPosition {};
+
+    size_t id1 = ComponentType::id<LocalPosition>();
+    size_t id2 = ComponentType::id<LocalPosition>();
 
     REQUIRE(id1 == id2);
 }
@@ -21,8 +18,11 @@ TEST_CASE(
     "ComponentType returns different ids for different types",
     "[unit][component_type]"
 ) {
-    size_t posId = ComponentType::id<Position>();
-    size_t velId = ComponentType::id<Velocity>();
+    struct LocalPosition {};
+    struct LocalVelocity {};
+
+    size_t posId = ComponentType::id<LocalPosition>();
+    size_t velId = ComponentType::id<LocalVelocity>();
 
     REQUIRE(posId != velId);
 }
@@ -31,9 +31,12 @@ TEST_CASE(
     "ComponentType id remains stable across interleaved calls",
     "[unit][component_type]"
 ) {
-    size_t pos1 = ComponentType::id<Position>();
-    size_t vel1 = ComponentType::id<Velocity>();
-    size_t pos2 = ComponentType::id<Position>();
+    struct LocalPosition {};
+    struct LocalVelocity {};
+
+    size_t pos1 = ComponentType::id<LocalPosition>();
+    size_t vel1 = ComponentType::id<LocalVelocity>();
+    size_t pos2 = ComponentType::id<LocalPosition>();
 
     REQUIRE(pos1 == pos2);
     REQUIRE(pos1 != vel1);
@@ -43,8 +46,14 @@ TEST_CASE(
     "ComponentType assigns incrementing ids",
     "[unit][component_type]"
 ) {
-    size_t id1 = ComponentType::id<Health>();
-    size_t id2 = ComponentType::id<Special>();
+    struct LocalA {};
+    struct LocalB {};
+    struct LocalC {};
+
+    size_t id1 = ComponentType::id<LocalA>();
+    size_t id2 = ComponentType::id<LocalB>();
+    size_t id3 = ComponentType::id<LocalC>();
 
     REQUIRE(id2 == id1 + 1);
+    REQUIRE(id3 == id2 + 1);
 }
