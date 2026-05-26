54d3b86a2193219846b3e03dc2d02e7bd9e6e75a
IagoGdeMeira <iagomeiraifpr@gmail.com>
2026-05-26 16:13:20 -0300

    fix(game): adicionando mais formatos ao UIDrawer

--- src/game/src/UIDrawer.cpp
+++ src/game/src/UIDrawer.cpp
@@ -1,12 +1,16 @@
 #include "../include/UIDrawer/UIDrawer.h"
 
+#include "../render_formats/CircleRenderFormat.h"
 #include "../render_formats/FontRenderFormat.h"
+#include "../render_formats/RectangleRenderFormat.h"
 #include "../render_formats/TextureRenderFormat.h"
 
 UIDrawer::UIDrawer(Renderer& renderer) : renderer(renderer)
 {
-    this->addFormat(std::make_unique<TextureRenderFormat>(this->renderer));
+    this->addFormat(std::make_unique<CircleRenderFormat>(this->renderer));
     this->addFormat(std::make_unique<FontRenderFormat>(this->renderer));
+    this->addFormat(std::make_unique<RectangleRenderFormat>(this->renderer));
+    this->addFormat(std::make_unique<TextureRenderFormat>(this->renderer));
 }
 
 void UIDrawer::draw(RenderContext& ctx)
